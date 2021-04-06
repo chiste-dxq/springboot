8ca159577a05054a2be7f6a248bde9c3a7da3bcb
daixq <daixiaoqing@98du.com>
2021-04-06 09:56:06 +0800

    基础功能配置

--- src/main/java/com/sj/demo/common/base/BaseController.java
+++ src/main/java/com/sj/demo/common/base/BaseController.java
@@ -2,8 +2,13 @@ package com.sj.demo.common.base;
 
 import com.github.pagehelper.PageInfo;
 import com.sj.demo.common.domain.PageVo;
+import com.sj.demo.common.util.DateUtils;
 import com.sj.demo.common.util.ResultUtils;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 
+import java.beans.PropertyEditorSupport;
+import java.util.Date;
 import java.util.List;
 
 /*
@@ -12,6 +17,23 @@ import java.util.List;
  * @Description:
  **/
 public class BaseController {
+
+    /**
+     * 将前台传递过来的日期格式的字符串，自动转化为Date类型
+     */
+    @InitBinder
+    public void initBinder(WebDataBinder binder)
+    {
+        // Date 类型转换
+        binder.registerCustomEditor(Date.class, new PropertyEditorSupport()
+        {
+            @Override
+            public void setAsText(String text)
+            {
+                setValue(DateUtils.parseDate(text));
+            }
+        });
+    }
 
     public static PageVo getDataTable(List<?> list)
     {
